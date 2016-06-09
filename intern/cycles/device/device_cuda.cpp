8128f8de30aced3d7bf31d5d52c077f328bea30a
Thomas Dinges <blender@dingto.org>
2016-06-09 18:53:00 +0200

    Add half data type to CUDA device.
    Have to clearly check on this though before adding support for actual half textures on GPU, as CUDA (since Toolkit 7.5) only supports a half and half2 data type, no half4.

--- intern/cycles/device/device_cuda.cpp
+++ intern/cycles/device/device_cuda.cpp
@@ -536,6 +536,7 @@ public:
 			case TYPE_UINT: format = CU_AD_FORMAT_UNSIGNED_INT32; break;
 			case TYPE_INT: format = CU_AD_FORMAT_SIGNED_INT32; break;
 			case TYPE_FLOAT: format = CU_AD_FORMAT_FLOAT; break;
+			case TYPE_HALF: format = CU_AD_FORMAT_HALF; break;
 			default: assert(0); return;
 		}
 
