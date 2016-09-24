319db37ac15d1f29c284b014408ace62f0412568
Julian Eisel <eiseljulian@gmail.com>
2016-09-25 01:00:23 +0200

    Remove manipulator library for initial core merge
    Will continue work on them in 2.8 based custom_manipulators branch, there I can also work on making manipulator drawing based on OpenGL 3.2 core profile.

--- source/blender/windowmanager/manipulators/intern/wm_manipulator.c
+++ source/blender/windowmanager/manipulators/intern/wm_manipulator.c
@@ -120,12 +120,12 @@ wmManipulator *WM_manipulator_new(
 	manipulator->render_3d_intersection = render_3d_intersection;
 
 	/* XXX */
-	fix_linking_manipulator_arrow();
-	fix_linking_manipulator_arrow2d();
-	fix_linking_manipulator_cage();
-	fix_linking_manipulator_dial();
+//	fix_linking_manipulator_arrow();
+//	fix_linking_manipulator_arrow2d();
+//	fix_linking_manipulator_cage();
+//	fix_linking_manipulator_dial();
 //	fix_linking_manipulator_facemap();
-	fix_linking_manipulator_primitive();
+//	fix_linking_manipulator_primitive();
 
 	return manipulator;
 }
