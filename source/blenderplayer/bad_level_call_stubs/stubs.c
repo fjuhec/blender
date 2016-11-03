e2ccfdaa29d0876cdecb035f913e577f1a9ee7a4
Bastien Montagne <montagne29@wanadoo.fr>
2016-11-03 20:28:48 +0100

    Fix bplayer (c)

--- source/blenderplayer/bad_level_call_stubs/stubs.c
+++ source/blenderplayer/bad_level_call_stubs/stubs.c
@@ -347,8 +347,7 @@ void WM_device_HMD_state_set(const int device, const bool enable) RET_NONE
 float WM_device_HMD_IPD_get(void) RET_ZERO
 const char *WM_device_HMD_name_get(int index) RET_NULL
 const char *WM_device_HMD_vendor_get(int index) RET_NULL
-void WM_device_HMD_left_projection_matrix_get(float leftMatrix[4][4]) RET_NONE
-void WM_device_HMD_right_projection_matrix_get(float rightMatrix[4][4]) RET_NONE
+void WM_device_HMD_IPD_set(float value) RET_NONE
 #endif /* WITH_INPUT_HMD */
 
 void                WM_uilisttype_init(void) RET_NONE
