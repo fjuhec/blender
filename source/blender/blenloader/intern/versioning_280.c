77f4152124f42cd16e266eded6bea5a1d33ce1be
Antonio Vazquez <blendergit@gmail.com>
2017-04-01 11:13:03 +0200

    Move initialization code to new versioning file

--- source/blender/blenloader/intern/versioning_280.c
+++ source/blender/blenloader/intern/versioning_280.c
@@ -34,6 +34,7 @@
 #include "DNA_scene_types.h"
 #include "DNA_screen_types.h"
 #include "DNA_genfile.h"
+#include "DNA_gpencil_types.h"
 
 #include "BKE_blender.h"
 #include "BKE_collection.h"
@@ -41,8 +42,11 @@
 #include "BKE_layer.h"
 #include "BKE_main.h"
 #include "BKE_scene.h"
+#include "BKE_gpencil.h"
+#include "BKE_paint.h"
 
 #include "BLI_listbase.h"
+#include "BLI_math.h"
 #include "BLI_mempool.h"
 #include "BLI_string.h"
 
@@ -256,5 +260,47 @@ void blo_do_versions_280(FileData *fd, Library *UNUSED(lib), Main *main)
 				}
 			}
 		}
+		/* ------- convert grease pencil palettes to blender palettes --------------- */
+		if (!DNA_struct_elem_find(fd->filesdna, "bGPDstroke", "Palette", "*palette")) {
+			for (bGPdata *gpd = main->gpencil.first; gpd; gpd = gpd->id.next) {
+				/* first create all palettes and colors */
+				Palette *first = NULL;
+				for (bGPDpalette *oldpalette = gpd->palettes.first; oldpalette; oldpalette = oldpalette->next) {
+					/* create palette */
+					Palette *newpalette = BKE_palette_add(main, oldpalette->info);
+					/* save first to use later */
+					if (first == NULL) {
+						first = newpalette;
+					}
+					/* enable fake user by default */
+					id_fake_user_set(&newpalette->id);
+
+					for (bGPDpalettecolor *oldcolor = oldpalette->colors.first; oldcolor; oldcolor = oldcolor->next) {
+						PaletteColor *newcolor = BKE_palette_color_add_name(newpalette, oldcolor->info);
+						/* set color attributes */
+						copy_v4_v4(newcolor->rgb, oldcolor->color);
+						copy_v4_v4(newcolor->fill, oldcolor->fill);
+						newcolor->flag = oldcolor->flag;
+					}
+					/* set first color active by default */
+					if (!BLI_listbase_is_empty(&newpalette->colors)) {
+						newpalette->active_color = 0;
+					}
+				}
+				/* second, assign the palette and the color (always to first palette) */
+				for (bGPDlayer *gpl = gpd->layers.first; gpl; gpl = gpl->next) {
+					for (bGPDframe *gpf = gpl->frames.first; gpf; gpf = gpf->next) {
+						for (bGPDstroke *gps = gpf->strokes.first; gps; gps = gps->next) {
+							Palette *palette = first;
+							PaletteColor *palcolor = BKE_palette_color_getbyname(first, gps->colorname);
+
+							gps->palette = palette;
+							gps->palcolor = palcolor;
+						}
+					}
+				}
+			}
+		}
+		/* ------- end grease pencil palettes conversion --------------- */
 	}
 }
