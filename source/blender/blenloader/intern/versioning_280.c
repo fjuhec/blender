52c46ceede6c8454e4a40cf69fd22a663ef7d612
Julian Eisel <eiseljulian@gmail.com>
2017-05-05 21:42:22 +0200

    Fix unfreed IDProperties, caused by branch merges

--- source/blender/blenloader/intern/versioning_280.c
+++ source/blender/blenloader/intern/versioning_280.c
@@ -286,12 +286,6 @@ void do_versions_after_linking_280(Main *main)
 	}
 
 	if (!MAIN_VERSION_ATLEAST(main, 280, 0)) {
-		IDPropertyTemplate val = {0};
-		for (Scene *scene = main->scene.first; scene; scene = scene->id.next) {
-			scene->collection_properties = IDP_New(IDP_GROUP, &val, ROOT_PROP);
-			BKE_layer_collection_engine_settings_create(scene->collection_properties);
-		}
-
 		for (bScreen *screen = main->screen.first; screen; screen = screen->id.next) {
 			for (ScrArea *sa = screen->areabase.first; sa; sa = sa->next) {
 				for (SpaceLink *sl = sa->spacedata.first; sl; sl = sl->next) {
