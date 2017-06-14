9fceef3895d41d121002b49c229e7b16928bb955
Antonio Vazquez <blendergit@gmail.com>
2017-06-14 17:05:12 +0200

    UI: Change icons for lock axis

--- source/blender/makesrna/intern/rna_sculpt_paint.c
+++ source/blender/makesrna/intern/rna_sculpt_paint.c
@@ -75,9 +75,9 @@ EnumPropertyItem rna_enum_gpencil_sculpt_brush_items[] = {
 
 EnumPropertyItem rna_enum_gpencil_lockaxis_items[] = {
 	{ GP_LOCKAXIS_NONE, "GP_LOCKAXIS_NONE", ICON_UNLOCKED, "", "Strokes unlocked" },
-	{ GP_LOCKAXIS_X, "GP_LOCKAXIS_X", ICON_MANIPUL, "X", "Project strokes to plane locked to X" },
-	{ GP_LOCKAXIS_Y, "GP_LOCKAXIS_Y", ICON_MANIPUL, "Y", "Project strokes to plane locked to Y" },
-	{ GP_LOCKAXIS_Z, "GP_LOCKAXIS_Z", ICON_MANIPUL, "Z", "Project strokes to plane locked to Z" },
+	{ GP_LOCKAXIS_X, "GP_LOCKAXIS_X", ICON_NDOF_DOM, "X", "Project strokes to plane locked to X" },
+	{ GP_LOCKAXIS_Y, "GP_LOCKAXIS_Y", ICON_NDOF_DOM, "Y", "Project strokes to plane locked to Y" },
+	{ GP_LOCKAXIS_Z, "GP_LOCKAXIS_Z", ICON_NDOF_DOM, "Z", "Project strokes to plane locked to Z" },
 	{ 0, NULL, 0, NULL, NULL }
 };
 
