9f7f310e256a0f88ec14636efa5402d8a5021b43
Antonio Vazquez <blendergit@gmail.com>
2017-08-07 13:14:51 +0200

    Fix z-depth problems in wave modifier

--- source/blender/draw/engines/gpencil/shaders/gpencil_wave_frag.glsl
+++ source/blender/draw/engines/gpencil/shaders/gpencil_wave_frag.glsl
@@ -19,20 +19,22 @@ void main()
 {
 	vec4 outcolor;
 	ivec2 uv = ivec2(gl_FragCoord.xy);
-	float stroke_depth = texelFetch(strokeDepth, uv, 0).r;
-	gl_FragDepth = stroke_depth;
+	float stroke_depth;
 
 	float value;
 	if (orientation == HORIZONTAL) {
 		float pval = (uv.x * M_PI) / wsize[0];
 		value = amplitude * sin((period * pval) + phase);
 		outcolor = texelFetch(strokeColor, ivec2(uv.x, uv.y + value), 0);
+		stroke_depth = texelFetch(strokeDepth, ivec2(uv.x, uv.y + value), 0).r;
 	}
 	else {
 		float pval = (uv.y * M_PI) / wsize[1];
 		value = amplitude * sin((period * pval) + phase);
 		outcolor = texelFetch(strokeColor, ivec2(uv.x + value, uv.y), 0);
+		stroke_depth = texelFetch(strokeDepth, ivec2(uv.x + value, uv.y), 0).r;
 	}
 
 	FragColor = outcolor;
+	gl_FragDepth = stroke_depth;
 }
