715c58d6bfffe72c6acdae942c2a4dac2b8f6957
Antonio Vazquez <blendergit@gmail.com>
2017-11-14 09:53:38 +0100

    Cleanup: Remove unused code

--- source/blender/modifiers/intern/MOD_gpencilsmooth.c
+++ source/blender/modifiers/intern/MOD_gpencilsmooth.c
@@ -30,22 +30,13 @@
 
 #include <stdio.h>
 
-#include "BLI_blenlib.h"
-#include "BLI_utildefines.h"
-#include "BLI_math_vector.h"
-#include "BLI_rand.h"
-
-#include "DNA_scene_types.h"
 #include "DNA_object_types.h"
 #include "DNA_gpencil_types.h"
 #include "DNA_modifier_types.h"
 
 #include "BKE_context.h"
-#include "BKE_global.h"
 #include "BKE_deform.h"
 #include "BKE_gpencil.h"
-#include "BKE_modifier.h"
-#include "BKE_object.h"
 
 #include "DEG_depsgraph.h"
 
