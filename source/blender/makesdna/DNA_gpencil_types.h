0891946c249cc7b868ae29c557c5f7171a38c5bc
Antonio Vazquez <blendergit@gmail.com>
2017-12-05 10:28:04 +0100

    Change default pixfactor to 1000

--- source/blender/makesdna/DNA_gpencil_types.h
+++ source/blender/makesdna/DNA_gpencil_types.h
@@ -40,7 +40,7 @@ struct GHash;
 
 /* TODO: add size as userprefs parameter */
 #define GP_OBGPENCIL_DEFAULT_SIZE  0.2f 
-#define GP_DEFAULT_PIX_FACTOR 500 
+#define GP_DEFAULT_PIX_FACTOR 1000 
 #define GP_DEFAULT_GRID_SIZE 100 
 
 /* ***************************************** */
