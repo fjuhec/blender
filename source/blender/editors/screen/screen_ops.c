93edc452920870bc02ff44100be4aa41df2b6fbb
Julian Eisel <eiseljulian@gmail.com>
2018-02-22 17:53:42 +0100

    UI: Increase default size of User-Preferences window
    Increased it by factor 1.28 (so we get a width of 1024px).
    WM_window_open_temp will ensure the window fits into the screen, so didn't need to ensure that.

--- source/blender/editors/screen/screen_ops.c
+++ source/blender/editors/screen/screen_ops.c
@@ -4021,9 +4021,9 @@ static void SCREEN_OT_back_to_previous(struct wmOperatorType *ot)
 
 static int userpref_show_invoke(bContext *C, wmOperator *op, const wmEvent *event)
 {
-	int sizex = 800 * UI_DPI_FAC;
-	int sizey = 480 * UI_DPI_FAC;
-	
+	const int sizex = 1024 * UI_DPI_FAC;
+	const int sizey = 614 * UI_DPI_FAC;
+
 	/* changes context! */
 	if (WM_window_open_temp(C, event->x, event->y, sizex, sizey, WM_WINDOW_USERPREFS) != NULL) {
 		return OPERATOR_FINISHED;
